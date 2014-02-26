bf526196a36f638cb09af998820afde4e2acc506
leodido <leodidonato@gmail.com>
2014-02-26 02:09:38 +0100

    Fix for maximum terminal width on Windows terminals

--- R/ncols.r
+++ R/ncols.r
@@ -1,15 +1,21 @@
-#' Computes the number of terminal window columns.
+#' @title
+#' Computes the maximum width of terminal window.
 #'
-#' @param     set_option    whether to automatically set the R terminal width to the system terminal number of columns [default TRUE]
-#' @return    Number of terminal window columns
+#' @description
+#' \code{\link{ncols}} detects the maximum number of the columns of the current terminal window.
+#' Note that columns are normally the same as characters except in CJK languages.
+#'
+#' @param     set_option    whether to automatically set the R terminal width to the system terminal maximum number of columns [default TRUE]
+#' @return    The number of terminal window columns
 #' @export
 ncols <- function(set_option = TRUE) {
+  sysname <- Sys.info()['sysname']
   if ((ncol <- Sys.getenv('COLUMNS')) != '') {
     ncol <- as.integer(ncol)
     if (set_option) {
       options(width = ncol) # FIXME: -1 ?
     }
-  } else if (Sys.info()['sysname'] == 'Darwin') {
+  } else if (sysname == 'Darwin') {
     # NOTE: I think this is a mac-only stty output format.
     # TODO: Need to prevent this from executing when under GUI
     output = tryCatch(system('stty -a', intern = TRUE), error = I)
@@ -20,6 +26,8 @@ ncols <- function(set_option = TRUE) {
       }
     }
     rm(output)
+  } else { # e.g., else if (sysname == Windows)
+    ncol <- getOption('width')
   }
 
   ncol
