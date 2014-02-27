0cb5574178d29524e18f05e906e3123d572d4e09
leodido <leodidonato@gmail.com>
2014-02-27 02:22:36 +0100

    Function ncols modified

--- R/ncols.r
+++ R/ncols.r
@@ -10,24 +10,15 @@
 #' @export
 ncols <- function(set_option = TRUE) {
   sysname <- Sys.info()['sysname']
-  if (sysname == 'Linux') {
-    if ((ncol <- Sys.getenv('COLUMNS')) != '') {
+  if ((ncol <- Sys.getenv('COLUMNS')) != '') {
       ncol <- as.integer(ncol)
-    } else {
-      output <- tryCatch(system('tput cols', intern = TRUE), error = I)
-      if (length(output) > 0) {
-        ncol <- as.integer(sub('([0-9]+)', '\\1', output[1]))
+      if (set_option) {
+        options(width = ncol)
       }
-    }
-    if (set_option) {
-      options(width = ncol)
-    }
-  } else if (sysname == 'Darwin') {
-    # NOTE: I think this is a mac-only stty output format.
-    # TODO: Need to prevent this from executing when under GUI
-    output <- tryCatch(system('stty -a', intern = TRUE), error = I)
+  } else if (sysname == 'Darwin' || sysname == 'Linux') {
+    output <- tryCatch(system('tput cols', intern = TRUE), error = I)
     if (length(output) > 0) {
-      ncol <- as.integer(sub('.* ([0-9]+) column.*', '\\1', output[1]))
+      ncol <- as.integer(sub('([0-9]+)', '\\1', output[1]))
       if (is.finite(ncol) && ncol > 0 && set_option) {
         options(width = ncol)
       }
