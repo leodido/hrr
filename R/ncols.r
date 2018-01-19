28f871096090f9b8ce410d59260860ce9edad1eb
leodido <leodidonato@gmail.com>
2018-01-19 01:52:45 +0100

    Fix: Removing cross-platform specific checks

--- R/ncols.r
+++ R/ncols.r
@@ -5,30 +5,29 @@
 #' \code{\link{ncols}} detects the maximum number of columns of the current terminal window.
 #' Note that columns are normally the same as characters except in CJK languages.
 #'
-#' @param     set_option    whether to automatically set the R terminal width to the system terminal maximum number of columns [default \code{TRUE}]
+#' @param     set_option    whether to automatically enforce the R terminal width to the system terminal maximum number of columns [default \code{TRUE}]
 #' @return    The number of terminal window columns.
 #' @export
 ncols <- function(set_option = TRUE) {
-  sysname <- Sys.info()['sysname']
-  if ((ncol <- Sys.getenv('COLUMNS')) != '') {
-    ncol <- as.integer(ncol)
-    if (set_option) {
-      options(width = ncol)
-    }
-    return(ncol)
-  } else if ((sysname == 'Darwin' || sysname == 'Linux') && Sys.getenv('TERM') != '') {
-    output <- tryCatch(system2("tput", "cols", stdout = TRUE, stderr = TRUE))
-    if (is.null(attr(output, "status"))) {
-      ncol <- as.integer(sub('([0-9]+)', '\\1', output[1]))
-      if (is.finite(ncol) && ncol > 0 && set_option) {
-        options(width = ncol)
+  # default/fallback
+  ncol <- getOption('width')
+
+  cols <- Sys.getenv('COLUMNS')
+  if (nzchar(cols)) {
+    ncol <- as.integer(cols)
+  } else if (nzchar(Sys.getenv('TERM'))) {
+    output <- tryCatch(system2('tput', 'cols', stdout = TRUE, stderr = TRUE))
+    if (!is.null(output) && is.null(attr(output, 'status'))) {
+      temp <- as.integer(sub('([0-9]+)', '\\1', output[1]))
+      if (is.finite(temp) && temp > 0) {
+        ncol <- temp
       }
     }
     rm(output)
-    return(ncol)
-  } else if (sysname == 'Windows') {
-    return(getOption('width') - 1)
   }
-  # fallback, should work also for Windows and various IDE (e.g. RStudio)
-  return(getOption('width'))
+
+  if (set_option) {
+    options(width = ncol)
+  }
+  return(ncol)
 }
